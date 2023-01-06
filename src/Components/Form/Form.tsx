6676f3a4c2e900f6a3c1d548abb601dabe1ab81f
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-01-06 15:46:50 +0100

    adds style to the form

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import './Form.css';
 
 const Form = () => {
+  const inputForm = document.querySelector<HTMLFormElement>('.inputForm');
+  const name = document.querySelector<HTMLInputElement>('#name');
+  const breed = document.querySelector<HTMLInputElement>('#breed');
+  const birthData = document.querySelector<HTMLInputElement>('#dateOfBirth')
+  
   return (
-    <form>
-      <label htmlFor="title">Name</label>
-      <input type="text" id="name" />
-      <label htmlFor="title">Breed</label>
-      <input type="text" id="breed" />
-      <label htmlFor="title">Date of Birth</label>
-      <input type="text" id="breed" />
-      <input type="submit" value="Register a New Puppy"/>
+    <form className="inputForm">
+      <label className="labelForm" htmlFor="name">Name</label>
+      <input className="inputFormText" type="text" id="name" />
+      <label className="labelForm" htmlFor="breed">Breed</label>
+      <input className="inputFormText" type="text" id="breed" />
+      <label className="labelForm" htmlFor="dateOfBirth">Date of Birth</label>
+      <input className="inputFormText" type="text" id="dateOfBirth" />
+      <input className="inputFormSubmit" type="submit" value="Register a New Puppy"/>
     </form>
   )
 }
