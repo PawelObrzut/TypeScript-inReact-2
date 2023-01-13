8b64f5901333f73648d103de171f8c7e2d1b38da
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-01-13 12:45:59 +0100

    adds breed options

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent, useRef } from 'react'
+import React, { MouseEvent, useRef, useState } from 'react'
 import './Form.css';
 
 interface FromProps {
@@ -6,19 +6,40 @@ interface FromProps {
 }
 
 const Form = ({ addNewPuppy }: FromProps) => {
+  const breedList: string[] = [
+    '',
+    'Buldog', 
+    'Chow chow', 
+    'Poodle', 
+    'Husky', 
+    'Dalmatian', 
+    'Doberman', 
+    'Golden Retriever', 
+    'Pug'
+  ];
  
+  const [breed, setBreed] = useState('');
   const inputName = useRef<HTMLInputElement>(null)
-  const inputBreed = useRef<HTMLInputElement>(null)
   const inputBirthDate = useRef<HTMLInputElement | null>(null)
   const inputForm = document.querySelector<HTMLFormElement>('.inputForm');
+
+  const selectHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+    setBreed(() => event.target.value);
+  }
   
   const handleSubmit = (event: MouseEvent<HTMLInputElement>) => {
     event.preventDefault();
     const name = inputName.current?.value;
-    const breed =  inputBreed.current?.value
     const birthDate = inputBirthDate?.current?.value.toString()
     addNewPuppy(name, breed, birthDate);
     inputForm?.reset();
+  }
+
+  const breedOption = (options: string[]): JSX.Element[] => {
+    return options.map(option => (
+      <option key={option} value={option} >{option}</option>
+    ))
   }
  
   return (
@@ -26,7 +47,9 @@ const Form = ({ addNewPuppy }: FromProps) => {
       <label className="labelForm" htmlFor="name">Name</label>
       <input className="inputFormText" type="text" id="name" ref={inputName}/> 
       <label className="labelForm" htmlFor="breed">Breed</label>
-      <input className="inputFormText" type="text" id="breed" ref={inputBreed}/>
+      <select className="inputFromSelect" onChange={selectHandler}>
+        {breedOption(breedList)}
+      </select>
       <label className="labelForm" htmlFor="dateOfBirth">Date of Birth</label>
       <input className="inputFormText" type="date" id="dateOfBirth" ref={inputBirthDate} />
       <input className="inputFormSubmit" type="submit" value="Register New Puppy" onClick={handleSubmit} />
